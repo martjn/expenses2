912e8b59c646ed10e1d41febfab70cd23e997912
martjn <mingdev77@gmail.com>
2023-02-06 15:21:34 +0200

    motherlode-3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import './ExpenseItem.css'
 
+import ExpenseDate from './ExpenseDate.js';
+
 function ExpenseItem(props) {
-    console.log(props)
+
 
     return (
         <div className="expense-item">
-            <div>{props.expenseData.date.toString()}</div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
