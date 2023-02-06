cb5aaf5da8d973b6aeaad73b658421bf0d8c71bd
martjn <mingdev77@gmail.com>
2023-02-06 15:13:54 +0200

    motherlode-3

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import './App.css';
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {
+            date : new Date(2023, 0, 10),
+            title : 'New Book',
+            price : 30.99
+        },
+        {
+            date : new Date(2023, 0, 10),
+            title : 'New jeans',
+            price : 99.99
+        }
+    ]
+
+
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
+      <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+      <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
     </div>
   );
 }
