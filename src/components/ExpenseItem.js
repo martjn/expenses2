8afef7efb4eb8a506d983a7dce0882349a88a05e
martjn <mingdev77@gmail.com>
2023-02-06 16:07:05 +0200

    motherlode-6.2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
 
 import ExpenseDate from './ExpenseDate.js';
+import Card from './Card.js';
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
