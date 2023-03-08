609026c812695910c8671777a12f6687f3d969d7
Martin Toming <58991701+martjn@users.noreply.github.com>
2023-03-08 10:46:54 +0200

    Add files via upload

---
+++ cypress/e2e/expenses.cy.js
@@ -0,0 +1,34 @@
+describe('Expenses', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display 1 item in expenses list', () => {
+    cy.get('.expenses-list li').should('have.length', 1)
+    cy.get('.expenses-list li h2').first().should('have.text', 'New hat')
+  })
+
+  it('can add a new expense', () => {
+    const title = 'New boots'
+    const amount = 70
+    const date = '2023-01-01'
+
+    cy.contains('Add New Expense').click()
+    cy.get('form #title').type(`${title}`)
+    cy.get('form #amount').type(`${amount}`)
+    cy.get('form #date').type(`${date}`)
+    cy.contains('Add Expense').click()
+
+    cy.get('.expenses-list li h2').first().should('have.text', 'New hat')
+
+  })
+
+  it('can filter expenses by year', () => {
+    cy.get('.expenses-filter__control select').select(1)
+    cy.get('.expenses-list li').should('have.length', 2)
+    cy.get('.expenses-list li h2').first().should('have.text', 'New Book')
+
+    cy.get('.expenses-filter__control select').select(2)
+    cy.get('.expenses-list li').should('have.length', 0)
+  })
+})
